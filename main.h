a9db3f1fb6ebd968cbd3b4feb973e438a13b38bb
Andres Henderson <ahenderson.holberton@gmail.com>
2021-11-06 16:11:37 -0700

    added struct to main

--- main.h
+++ main.h
@@ -2,6 +2,23 @@
 #define MAIN_H
 #include <stdlib.h>
 #include <stdarg.h>
+
+/**
+ * struct list - c, f
+ * @c: character to check
+ * @f: Pointer to function
+ */
+
+
+typedef struct list
+{
+	char c;
+	int (*f)(va_list);
+} li;
+
+
+
+
 int _strlen(char *s);
 int _printf(const char *format, ...);
 #endif
