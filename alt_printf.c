b26054beeedfe8b676663b8ffb1706fbbebdef7f
Andres Henderson <ahenderson.holberton@gmail.com>
2021-11-07 14:07:54 -0800

    another way

---
+++ alt_printf.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdarg.h>
+
+/**
+ * _printf - Prints what it is given
+ * @format: String that holds what is going to print
+ * Return: length of printed
+ */
+int _printf(const char *format, ...)
+{
+	unsigned int i;
+
+	va_list to_print; /** type for iterating args*/
+	va_start(to_print, format); /** start iterating args */
+
+	/** check if format not null & has specifier*/
+	if (!format && (format[0] == '%' && format[1] == '\n'))
+		return (-1);
+	/** start to check if there are % and specifiers*/
+	for (i = 0; format[i] != 0; i++)
+	{/**print if no specifier is found*/
+		if (format[i] != '%')
+			_putchar(format[i]);
+		else if (format[i] == '%' && format[i + 1] == '%')/**check if next is %*/
+			_putchar('%');
+		else if (format[i] == '%' && format[i + 1] != '%')
+		{/**call a foo that prints what we want using the va_arg(to_print,format)*/
+
+
+
+		
+
+
+
+			
+
+
+
+
+}
