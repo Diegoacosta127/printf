631f65f2ea7a4420e8e78a92f06bbe5c2b894417
Andres Henderson <ahenderson.holberton@gmail.com>
2021-11-08 08:45:26 -0800

    added ap to main

--- test/main.h
+++ test/main.h
@@ -18,10 +18,10 @@ typedef struct list
 } all_cases;
 
 int selector(char str, va_list ap);
-int function_c(va_list);
-int function_s(va_list);
-int function_d(va_list);
-int function_i(va_list);
+int function_c(va_list ap);
+int function_s(va_list ap);
+int function_d(va_list ap);
+int function_i(va_list ap);
 int function_por(va_list);
 int _strlen(char *s);
 int _putchar(char c);
