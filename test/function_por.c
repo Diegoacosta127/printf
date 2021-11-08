bee6319961d2557c0b8ef12632c6d22f8a218811
Diego Monfort <diegomonfort12346@gmail.com>
2021-11-08 08:27:45 -0800

    defined several cases

---
+++ test/function_por.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*function_por -print_percent
+*@arg: argument
+*Return: 0
+*/
+int function_por(va_list arg)
+{
+	(void)arg;
+
+	_putchar('%');
+	return (1);
+}
