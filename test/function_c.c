5873a84753061de15bb93b64f387eb34cc1484f0
Andres Henderson <ahenderson.holberton@gmail.com>
2021-11-07 17:25:03 -0800

    added foo to print a char

--- test/function_c.c
+++ test/function_c.c
@@ -2,11 +2,19 @@
 
 /**
  * function_c - Prints a single char
- * 
- *
+ * @a_char: the format to print
+ * Return: Length of printed stuff. One for this foo
  */
 
-int function_c(va_list)
+int function_c(va_list a_char)
 {
+
+	char single_char = va_arg(a_char, int);
+	/**Prints that single char*/
+	_putchar(single_char);
+
+	/** returns 1 because its a single char*/
+	return (1);
+}
 
 
